d8bd88a0535ee0a09716187ca38440781208bded
Konrad Pagacz <konrad.pagacz@gmail.com>
2019-12-18 17:24:14 +0100

    zajecia-12

---
+++ wstep-do-programowania/zajecia-12/sum-of-digits.cpp
@@ -0,0 +1,26 @@
+// Napisać program obliczający sumę cyfr liczby naturalnej podanej jako parametr. Zadanie 
+// rozwiązać za pomocą funkcji rekurencyjnej. [podpowiedź: cyfrę jedności liczby A uzyskujemy 
+// obliczając resztę z dzielenia jej przez 10; liczbę A skracamy o końcową cyfrę wykonując 
+// dzielenie całkowite tej liczby przez 10; czynności powtarzamy do momentu uzyskania zera ] 
+
+#include <iostream>
+
+int digit_sum(long long a);
+
+int main() {
+    // input
+    std::cout << "Input an integer: ";
+    long long a;
+    std::cin >> a;
+
+    std::cout << "Sum of digits: " << digit_sum(a);
+    return 0;
+}
+
+int digit_sum(long long a) {
+    if (a == 0) {
+        return 0;
+    }
+
+    return a - (a % 10) + digit_sum(a % 10);
+}
