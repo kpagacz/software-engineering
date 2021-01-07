76f4759b4411d2c4cb6381f5bac24a6412523841
Konrad Pagacz <konrad.pagacz@contractors.roche.com>
2021-01-07 15:16:29 +0100

    numeryczne-projekt1

--- metody-numeryczne/projekt-1/projekt-1.cpp
+++ metody-numeryczne/projekt-1/projekt-1.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <math.h>
 #include <fstream>
+#include <string>
 
 
 double equation_11(double x) {
@@ -12,12 +13,24 @@ double equation_11_der(double x) {
     return pow(2, x) * (x * log(2) + 1);
 }
 
-double equation_11_sol() {
+double equation_11_sol(double x) {
     return 0.6411857445049859;
 }
 
 double algebraic_equation(double x) {
     return (x - 1.7) * (x + 0.3) * (x - 1.1);
+}
+
+double algebraic_equation_der(double x) {
+    return 1.03 + x * (-5.0 + 3 * x);
+}
+
+double algebraic_equation_sol(double x) {
+    // I will be checking only two roots, not three
+    if (x > 1.7)
+        return 1.7;
+    else if (x < -0.3)
+        return -0.3;
 }
 
 double multipoint_formula_N(double x, double func(double), double diff(double)) {
@@ -27,16 +40,16 @@ double multipoint_formula_N(double x, double func(double), double diff(double)) 
     ));
 }
 
-auto root_newton(double x, double eps, double func(double), double diff(double)) {
+auto root_newton(double x_start, double eps, double func(double), double diff(double)) {
     int counter = 0;
     double h;
     do {
-        h = -(func(x) / diff(x));
-        x += h;
+        h = -(func(x_start) / diff(x_start));
+        x_start += h;
         counter++;
     } while (abs(h) > eps);
    
-    return std::pair(x, counter);
+    return std::make_pair(x_start, counter);
 }
 
 auto root_multipoint(double x_start, double eps, double func(double), double diff(double)) {
@@ -49,15 +62,59 @@ auto root_multipoint(double x_start, double eps, double func(double), double dif
         counter++;
     } while(difference > eps);
 
-    return std::pair(x_start, counter);
+    return std::make_pair(new_x, counter);
 }
 
-void run_experment(std::ofstream& results_file) {
+void run_experiment(std::ofstream& results_file, double x_start, double eps, double func(double), double diff(double), double solution(double)) {
+    auto multi_res = root_multipoint(x_start, eps, func, diff);
+    auto newton_res = root_newton(x_start, eps, func, diff);
 
+    double sol = solution(x_start);
+
+    results_file << "wielopunktowa," <<
+        std::fixed << std::setprecision(3) << x_start << "," <<
+        std::scientific << std::setprecision(2) << eps << "," <<
+        multi_res.second << "," <<
+        std::fixed << std::setprecision(16) << multi_res.first << "," <<
+        std::scientific << std::setprecision(3) << abs(multi_res.first - sol) << "," <<
+        std::scientific << std::setprecision(3) << abs(multi_res.first - sol) / abs(multi_res.first) << "\n";
+
+    results_file << "Newtona," <<
+        std::fixed << std::setprecision(3) << x_start << "," <<
+        std::scientific << std::setprecision(2) << eps << "," <<
+        newton_res.second << "," <<
+        std::fixed << std::setprecision(16) << newton_res.first << "," <<
+        std::scientific << std::setprecision(3) << abs(newton_res.first - sol) << "," <<
+        std::scientific << std::setprecision(3) << abs(newton_res.first - sol) / abs(newton_res.first) << "\n";
 }
 
 int main() {
+    std::string file_name = "output.csv";
+    std::ofstream results;
+    results.open(file_name, std::ofstream::out);
+    results << "Metoda,Poczatkowe x,Epsilon,Liczba iteracji,Wynik,Blad bezwzgledny,Blad wzgledny\n";
 
+    double x_start = 10, eps = 0.001;
+    //auto res = root_multipoint(x_start, eps, equation_11, equation_11_der);
+    //std::cout << res.first << " " << res.second << "\n";
 
+    run_experiment(results, x_start, eps, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 10, 0.1, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 100, 0.1, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 1000, 0.1, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 10000, 0.1, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 100000, 0.1, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 10, 0.001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 100, 0.001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 1000, 0.001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 10000, 0.001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 100000, 0.001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 10, 0.0000001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 100, 0.0000001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 1000, 0.0000001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 10000, 0.0000001, equation_11, equation_11_der, equation_11_sol);
+    run_experiment(results, 100000, 0.0000001, equation_11, equation_11_der, equation_11_sol);
+
+    results.close();
 	return 0;
 }
