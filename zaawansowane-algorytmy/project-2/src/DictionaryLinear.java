3e89f30af67e542ac0c00d4cbe5b0ede61f8a354
Konrad Pagacz <konrad.pagacz@gmail.com>
2021-04-21 23:33:53 +0200

    zaawansowane-project-2

--- zaawansowane-algorytmy/project-2/src/DictionaryLinear.java
+++ zaawansowane-algorytmy/project-2/src/DictionaryLinear.java
@@ -18,12 +18,15 @@ public abstract class DictionaryLinear<K, V> {
   private record Pair<First, Second>(First first, Second second) { }
   private ArrayList<DictionaryElement<K, V>>[] data;
   private int currentBucketsNumber;
+  private int currentElementsNumber;
+  private int dictCoefficient = 4;
+  private int growCoefficient = 2;
 
   // Constructors
   public DictionaryLinear() {
-    int initialBucketsNumber = 128;
-    currentBucketsNumber = initialBucketsNumber;
-    data = new ArrayList[initialBucketsNumber];
+    currentBucketsNumber = 128;
+    currentElementsNumber = 0;
+    data = new ArrayList[currentBucketsNumber];
     for(ArrayList<DictionaryElement<K, V>> list : data) {
       list = new ArrayList<DictionaryElement<K, V>>();
     }
@@ -39,9 +42,24 @@ public abstract class DictionaryLinear<K, V> {
     }
   }
 
-  public final void insert(K key, V value) {}
+  public final void insert(K key, V value) {
+    Pair<Integer, Integer> indices = findIndex(key);
+    if(indices.second == -1) {
+      data[indices.first].add(new DictionaryElement<>(key, value));
+      ++currentElementsNumber;
+      if (currentElementsNumber > currentBucketsNumber * dictCoefficient) {
+        rehash(growCoefficient * currentBucketsNumber);
+      }
+    } else {
+      data[indices.first].get(indices.second).value = value;
+    }
+  }
 
   public final void delete(K key) {}
+
+  public final int size() {
+    return currentElementsNumber;
+  }
 
   @Override
   public final String toString() {
@@ -69,5 +87,21 @@ public abstract class DictionaryLinear<K, V> {
       }
     }
     return new Pair<>(hash(key), -1);
+  }
+
+  private void rehash(int newSize) {
+    currentBucketsNumber = newSize;
+    ArrayList<DictionaryElement<K, V>>[] newData = new ArrayList[currentBucketsNumber];
+    for(ArrayList<DictionaryElement<K, V>> list : newData) {
+      list = new ArrayList<DictionaryElement<K, V>>();
+    }
+
+    for(ArrayList<DictionaryElement<K, V>> list : data) {
+      for(DictionaryElement<K, V> element : list) {
+        newData[hash(key(element))].add(element);
+      }
+    }
+
+    data = newData;
   }
 }
