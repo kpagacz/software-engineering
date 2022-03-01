cb04a48ea3eee6055d573f372f4d4dd8d8ba9621
kpagacz <konrad.pagacz@gmail.com>
2022-03-01 14:40:21 +0100

    designing-user-interfaces-project-init

---
+++ designing-user-interfaces/project/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+
+const App = () => {
+  return <h1>My first react app!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
