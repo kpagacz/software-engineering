ca0634f4fa24f95926f13c117147e98e698552da
kpagacz <konrad.pagacz@gmail.com>
2022-03-22 15:31:09 +0100

    designing user interfaces

---
+++ designing-user-interfaces/project/src/components/ListElement.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <div className="box"></div>
+      <form>
+        <input type="text"></input>
+      </form>
+    </div>
+  );
+};
--- designing-user-interfaces/project/src/index.js
+++ designing-user-interfaces/project/src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import Home from "./views/Home.js";
 
 const App = () => {
-  return <h1>My first react app!</h1>;
-}
+  return <Home />;
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
