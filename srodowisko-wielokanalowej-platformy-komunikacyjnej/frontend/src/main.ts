c83cac9a8845012e6774ca663f05c9019e6ceea2
kpagacz <konrad.pagacz@gmail.com>
2022-04-05 14:45:02 +0200

    ansible

--- srodowisko-wielokanalowej-platformy-komunikacyjnej/frontend/src/main.ts
+++ srodowisko-wielokanalowej-platformy-komunikacyjnej/frontend/src/main.ts
@@ -1,13 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import io from 'socket.io-client';
-import * as config from '../../backend/config.js';
-
-const socket = io(config.api.url, {
-  reconnection: false,
-  transports: ["websocket", "polling"],
-  path: config.api.prefix + "/socket"
-});
 
 createApp(App).use(router).mount('#app')
