3e2d7c086160b6e9877e2f4878504539f65852bf
kpagacz <konrad.pagacz@gmail.com>
2022-05-05 09:58:30 +0200

    feat: basic to-do items animation

--- designing-user-interfaces/project/src/components/List.js
+++ designing-user-interfaces/project/src/components/List.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import ListElement from "./ListElement";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const List = () => {
   const [checked, setChecked] = useState([]);
@@ -26,49 +29,70 @@ const List = () => {
 
   const newListHandler = (event) => {
     event.preventDefault();
-    console.dir(event.target[0].value);
     setChecked([false, ...checked]);
     setTexts([event.target[0].value, ...texts]);
     event.target[0].value = "";
   };
 
+  const getItemsToDo = () => {
+    return (
+      <TransitionGroup className="todo-items">
+        {checked.map((check, index) => {
+          if (!check) {
+            const textHandler = onTextChangeHandler(index);
+            const checkboxHandler = onCheckboxChangeHandler(index);
+            return (
+              <CSSTransition key={index} timeout={300} classNames="todo-item">
+                <ListElement
+                  onTextChange={textHandler}
+                  onCheckboxChange={checkboxHandler}
+                  checked={check}
+                  text={texts[index]}
+                  key={index}
+                />
+              </CSSTransition>
+            );
+          }
+        })}
+      </TransitionGroup>
+    );
+  };
+
+  const getDoneItems = () => {
+    return checked.map((check, index) => {
+      if (check) {
+        const textHandler = onTextChangeHandler(index);
+        const checkboxHandler = onCheckboxChangeHandler(index);
+        return (
+          <ListElement
+            onTextChange={textHandler}
+            onCheckboxChange={checkboxHandler}
+            checked={check}
+            text={texts[index]}
+            key={index}
+            readOnly="readonly"
+          />
+        );
+      }
+    });
+  };
+
   return (
     <div>
       <form onSubmit={newListHandler}>
-        <input type="text" placeholder="Add another item..." autofocus></input>
+        <input
+          type="text"
+          placeholder="Add another item..."
+          autoFocus
+          required="required"
+        ></input>
+        <button type="submit">
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
       </form>
-      {checked.map((check, index) => {
-        if (!check) {
-          const textHandler = onTextChangeHandler(index);
-          const checkboxHandler = onCheckboxChangeHandler(index);
-          return (
-            <ListElement
-              onTextChange={textHandler}
-              onCheckboxChange={checkboxHandler}
-              checked={check}
-              text={texts[index]}
-              key={index}
-            />
-          );
-        }
-      })}
+      {getItemsToDo()}
       {checked.some((value) => value === true) && <div>Done:</div>}
-      {checked.map((check, index) => {
-        if (check) {
-          const textHandler = onTextChangeHandler(index);
-          const checkboxHandler = onCheckboxChangeHandler(index);
-          return (
-            <ListElement
-              onTextChange={textHandler}
-              onCheckboxChange={checkboxHandler}
-              checked={check}
-              text={texts[index]}
-              key={index}
-              readOnly="readonly"
-            />
-          );
-        }
-      })}
+      {getDoneItems()}
     </div>
   );
 };
--- designing-user-interfaces/project/src/index.js
+++ designing-user-interfaces/project/src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Home from "./views/Home.js";
 import SettingsView from "./views/SettingsView";
 import List from "./components/List";
+import style from "./index.css";
 
 const App = () => {
   return <List />;
